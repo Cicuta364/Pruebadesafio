e20ed312a3b045094eb424dadcaa6da8f8851a08
Cicuta364 <patricio.campos.u@gmail.com>
2015-12-21 22:40:53 -0300

    controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+Client.destroy_all
+User.destroy_all
+Acount.destroy_all
+User_Acount.destroy_all
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
