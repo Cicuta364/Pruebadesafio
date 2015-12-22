80a414ac6bb358af9d5e506799d464ef3064d017
Cicuta364 <patricio.campos.u@gmail.com>
2015-12-21 22:29:03 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :acounts
   devise_for :users
+
+  root 'acounts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
